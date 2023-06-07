d9c0854fdf90bc1cb4fe963efbb071f333d9e92d
yashsalesforce2706 <yash_java@outlook.com>
2023-06-06 22:48:04 -0400

    Microservices Assignment

--- Spring/Microspringbootreactive/src/main/java/com/example/springmicro/reactive/UserRepository.java
+++ Spring/Microspringbootreactive/src/main/java/com/example/springmicro/reactive/UserRepository.java
@@ -4,5 +4,3 @@ import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends ReactiveCrudRepository<User, String> {
-    Mono<User> findByName(String name);}
