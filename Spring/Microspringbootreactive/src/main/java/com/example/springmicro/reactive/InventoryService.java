2e4da0712a8fecd1307f04b10a44a000f95e57ab
yashsalesforce2706 <yash_java@outlook.com>
2023-06-12 22:38:07 -0400

    Microservices Assignment

--- Spring/Microspringbootreactive/src/main/java/com/example/springmicro/reactive/InventoryService.java
+++ Spring/Microspringbootreactive/src/main/java/com/example/springmicro/reactive/InventoryService.java
@@ -36,4 +36,43 @@ class InventoryService {
         return this.itemRepository.deleteById(id);
     }
 
+    Mono<Cart> addItemToCart(String cartId, String itemId) {
+        return this.cartRepository.findById(cartId)
+                .defaultIfEmpty(new Cart(cartId))
+                .flatMap(cart -> cart.getCartItems().stream()
+                        .filter(cartItem -> cartItem.getItem().getId().equals(itemId))
+                        .findAny()
+                        .map(cartItem -> {
+                            cartItem.increment();
+                            return Mono.just(cart);
+                        })
+                        .orElseGet(() -> {
+                            return this.itemRepository.findById(itemId)
+                                    .map(item -> new CartItem(item))
+                                    .map(cartItem -> {
+                                        cart.getCartItems().add(cartItem);
+                                        return cart;
+                                    });
+                        }))
+                .flatMap(cart -> this.cartRepository.save(cart));
+    }
+
+
+    Mono<Cart> removeOneFromCart(String cartId, String itemId) {
+        return this.cartRepository.findById(cartId)
+                .defaultIfEmpty(new Cart(cartId))
+                .flatMap(cart -> cart.getCartItems().stream()
+                        .filter(cartItem -> cartItem.getItem().getId().equals(itemId))
+                        .findAny()
+                        .map(cartItem -> {
+                            cartItem.decrement();
+                            return Mono.just(cart);
+                        })
+                        .orElse(Mono.empty()))
+                .map(cart -> new Cart(cart.getId(), cart.getCartItems().stream()
+                        .filter(cartItem -> cartItem.getQuantity() > 0)
+                        .collect(Collectors.toList())))
+                .flatMap(cart -> this.cartRepository.save(cart));
+    }
 }
+
